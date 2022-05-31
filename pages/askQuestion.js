886c90855dbb618f772337e6f033f65c0103aaf2
Syed_Zaidi <syedabdullah.7ctech@gmail.com>
2022-05-31 20:06:26 +0500

    updated

--- pages/askQuestion.js
+++ pages/askQuestion.js
@@ -9,11 +9,10 @@ const askQuestion = ({topics}) => {
 }
 export default askQuestion
 
-export async function getServerSideProps(context){
+export async function getServerSideProps(){
 
   const request = await axios.get(`${process.env.NEXT_PUBLIC_ASK_HIDAYA_GLOBAL}/public/topics/`)
   .then((x)=> x.data);
-  //questions/?limit=10&offset=${0}&search=${word}
   return {
     props: {
       topics:JSON.parse(JSON.stringify(request))
