99a47e59ab894a9a3ca5740814b6bd1661237926
Syed_Zaidi <syedabdullah.7ctech@gmail.com>
2022-05-31 20:15:32 +0500

    updated

--- pages/askQuestion.js
+++ pages/askQuestion.js
@@ -1,21 +1,9 @@
 import React from 'react'
-import AskQuestionLayout from '/components/layouts/AskQuestionLayout/'
-import axios from 'axios'
 
-const askQuestion = ({topics}) => {
+const askQuestion = () => {
   return (
-    <div><AskQuestionLayout topics={topics} /></div>
+    <div>askQuestion</div>
   )
 }
+
 export default askQuestion
-
-export async function getServerSideProps(){
-
-  const request = await axios.get(`${process.env.NEXT_PUBLIC_ASK_HIDAYA_GLOBAL}/public/topics/`)
-  .then((x)=> x.data);
-  return {
-    props: {
-      topics:JSON.parse(JSON.stringify(request))
-    },
-  }
-}
