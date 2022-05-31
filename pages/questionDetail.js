5344d84c855033d143b817281ae61b7f5d1a28fc
Syed_Zaidi <syedabdullah.7ctech@gmail.com>
2022-05-31 20:17:56 +0500

    updated

--- pages/questionDetail.js
+++ pages/questionDetail.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import axios from 'axios'
-import QuestionDetailLayout from '/components/layouts/QuestionDetailLayout/'
+import React from 'react';
+import axios from 'axios';
+import QuestionDetailLayout from '/components/layouts/QuestionDetailLayout';
 
 const questionDetail = ({questions, reference}) => {
   return (
@@ -9,8 +9,7 @@ const questionDetail = ({questions, reference}) => {
 }
 export default questionDetail
 
-export async function getServerSideProps(context) {
-    //console.log(context.query)
+export async function getServerSideProps() {
     const requestOne = await axios.get(`${process.env.NEXT_PUBLIC_ASK_HIDAYA_GLOBAL}/temp-questions/${context.query.id}`).then((res)=>res.data)
     const requestTwo = await axios.get(`${process.env.NEXT_PUBLIC_ASK_HIDAYA_GLOBAL}/reference/?question_id=${context.query.id}`).then((res)=>res.data)
     return {
